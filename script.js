36071cf1e0b7789d274f0aa5b09093bb5a8f91ca
BryanGK <bryangordonkrauss@gmail.com>
2021-02-18 17:59:36 -0800

    Completed UI functionality and state persist across reloads.

--- script.js
+++ script.js
@@ -1,65 +1,88 @@
-const submitBtn = document.querySelector('#submit-btn');
-const toDo = document.querySelector('#todo-field');
-const toDoList = document.querySelector('#todo-list')
-const done = document.querySelectorAll('.pending');
+window.onload = displayTodos;
 
-submitBtn.addEventListener('click', () => {
-    let listItem = toDo.value;
-    localStorage.setItem(`toDo${localStorage.length + 1}`, listItem);
-})
+document.querySelector("#submit-btn").addEventListener("click", () => {
+    saveTodo();
+    displayTodos();
+});
 
-//Displays the items in localStorage on the todo-list
-
-for (let i = 1; i <= localStorage.length; i++) {
-    const newListItem = document.createElement('li');
-    const doneButton = addButtonDone(i);
-    const clearButton = addButtonClear(i);
-    newListItem.append(localStorage.getItem(`toDo${i}`));
-    newListItem.append(doneButton);
-    newListItem.append(clearButton);
-    newListItem.classList.add('list-item');
-    newListItem.setAttribute('id', `toDo${i}`);
-    toDoList.append(newListItem);
-    console.log(localStorage.getItem(`toDo${i}`));
+function saveTodo() {
+    const todoInputElement = document.querySelector("#todo-field");
+    const todoText = todoInputElement.value;
+    let todoObject = JSON.stringify({ todoText, isCompleted: false });
+    localStorage.setItem(`${Date.now()}`, todoObject);
+    todoInputElement.value = "";
 }
 
-function addButtonDone(i) {
-    const doneButton = document.createElement('input');
-    doneButton.setAttribute('type', 'checkbox');
-    doneButton.classList.add('pending');
-    doneButton.addEventListener('click', strikethru);
+function displayTodos() {
+    const todoList = document.querySelector("#todo-list");
+    todoList.innerHTML = "";
+    const ids = getSavedTodosIds();
+
+    ids.forEach((id) => {
+        const newListItem = createTodo(id);
+        todoList.append(newListItem);
+    });
+}
+
+function getSavedTodosIds() {
+    return Object.keys(localStorage).sort((a, b) => {
+        return Number(a) - Number(b);
+    });
+}
+
+function createTodo(id) {
+    const todoListItem = document.createElement("li");
+    const doneButton = createToggleTodoButton(id);
+    const deleteButton = createDeleteButton(id);
+    const todoObject = localStorage.getItem(id);
+    const parsedTodoObject = JSON.parse(todoObject);
+    todoListItem.append(parsedTodoObject.todoText);
+    todoListItem.append(doneButton);
+    todoListItem.append(deleteButton);
+    checkStrikethru(parsedTodoObject, todoListItem, doneButton);
+    todoListItem.classList.add("list-item");
+    todoListItem.setAttribute("id", id);
+    return todoListItem;
+}
+
+function checkStrikethru(parsedTodoObject, todoListItem, doneButton) {
+    if (parsedTodoObject.isCompleted === true) {
+        todoListItem.classList.add("strike");
+        doneButton.checked = true;
+    }
+}
+
+function createDeleteButton(id) {
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "x";
+    deleteButton.classList.add("clear");
+    deleteButton.addEventListener("click", () => deleteTodo(id, deleteButton));
+    return deleteButton;
+}
+
+function deleteTodo(id, deleteButton) {
+    localStorage.removeItem(id);
+    deleteButton.parentElement.remove();
+}
+
+function createToggleTodoButton(id) {
+    const doneButton = document.createElement("input");
+    doneButton.setAttribute("type", "checkbox");
+    doneButton.classList.add("pending");
+    doneButton.addEventListener("click", () => strikethru(id, doneButton));
     return doneButton;
 }
 
-function strikethru() {
-    let item = this.closest('.list-item');
-    if (!item.classList.contains('strike')) {
-        item.classList.add('strike');
-    } else {
-        item.classList.remove('strike');
-    }
+function strikethru(id, doneButton) {
+    const item = doneButton.parentElement;
+    !item.classList.contains("strike")
+        ? item.classList.add("strike")
+        : item.classList.remove("strike");
+
+    const todoObject = JSON.parse(localStorage.getItem(id));
+    if (!todoObject.isCompleted) {
+        todoObject.isCompleted = true;
+    } else
+        todoObject.isCompleted = false;
+    localStorage.setItem(id, JSON.stringify(todoObject));
 }
-
-function addButtonClear(i) {
-    const clearButton = document.createElement('button');
-    clearButton.innerHTML = "x";
-    clearButton.classList.add('clear');
-    clearButton.setAttribute('id', `toDo${i}`);
-    clearButton.addEventListener('click', clearList);
-    return clearButton;
-}
-
-function clearList() {
-    let item = this.getAttribute('id');
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        if (key === item) {
-            localStorage.removeItem(key);
-        }
-    }
-    this.closest('.list-item').remove();
-}
-
-
-
-
